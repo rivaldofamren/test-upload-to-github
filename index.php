9dac891fb627427587e9467c957751aa60c38894
rivaldofamren <rivaldofamren712@gmail.com>
2024-02-28 12:59:05 +0700

    update index.php

--- index.php
+++ index.php
@@ -12,5 +12,9 @@
     Dolore ad numquam corporis ea beatae cum porro dignissimos dolorum quis excepturi at dolor neque nobis, itaque fugiat hic quia ipsa. Deleniti maiores in distinctio deserunt quod odio, dicta error?
     Labore totam provident atque beatae ea itaque consequuntur, impedit asperiores quas laudantium veritatis omnis. Voluptate repellendus quam quae, obcaecati ipsum aut quos aspernatur impedit, cupiditate, consectetur quod at culpa officiis?
     Rem corporis iste voluptatibus officiis. Sapiente adipisci ab beatae fuga cupiditate nisi unde quis aut quidem aliquid culpa nobis error doloremque nam modi assumenda, enim consequatur! Rerum culpa quae debitis.</h1>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
